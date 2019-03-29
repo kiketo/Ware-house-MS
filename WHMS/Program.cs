d9e45af21a7aaad486ef0a46407eac6ca373a02a
Kiril Kuzmanov <kiketo@gmail.com>
2019-03-29 16:37:58 +0200

    Start Up

---
+++ WHMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WHMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
