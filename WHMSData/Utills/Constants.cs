c2f7c4dbd4dcc76be38310eb1daacd9d01132e7c
Kiril Kuzmanov <kiketo@gmail.com>
2019-04-01 16:54:29 +0300

    deleted constants

--- WHMSData/Utills/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WHMSData.Utills
-{
-    public class Constants
-    {
-        //public const string serverName = "ADMIN-PC34"; //TODO: Ива сложи тук своя сървър и после този файл ще го допишем в .gitignore
-        public const string serverName = ".\\SQLEXPRESS";
-    }
-}
