22e91963fa7d674322226694b8432e395a9d6aea
Kiril Kuzmanov <kiketo@gmail.com>
2019-04-01 16:51:48 +0300

    revised

--- WHMSData/Migrations/20190401125636_marginToDouble.Designer.cs
+++ WHMSData/Migrations/20190401125636_marginToDouble.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WHMSData.Context;
 
 namespace WHMSData.Migrations
 {
     [DbContext(typeof(WHMSContext))]
-    partial class WHMSContextModelSnapshot : ModelSnapshot
+    [Migration("20190401125636_marginToDouble")]
+    partial class marginToDouble
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WHMSData/Migrations/20190401125636_marginToDouble.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WHMSData.Migrations
+{
+    public partial class marginToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "MarginInPercent",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MarginInPercent",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
