dc1a1fb220634f26cccbbabb5ebb37ee7c9ae038
Kiril Kuzmanov <kiketo@gmail.com>
2019-04-04 14:06:18 +0300

    IPartnerService created

--- WHMS.Services/Interfaces/IPartnerService.cs
+++ WHMS.Services/Interfaces/IPartnerService.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using WHMSData.Models;
 
 namespace WHMS.Services.Interfaces
 {
     public interface IPartnerService
     {
+        Partner Add(string partnerName, int vat = 0);
+
+        Partner Edit(string partnerName, string newPartnerName, int newVat = 0);
+
+        Partner Delete(string partnerName);
+
+        Partner Find(string partnerName);
     }
 }
