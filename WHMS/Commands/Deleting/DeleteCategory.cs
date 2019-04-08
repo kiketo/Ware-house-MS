14fe2bf7128ee867e7ff5cf59b94e2f29c704f6f
rhin <rhin3381@gmail.com>
2019-04-08 15:22:59 +0300

    deleteCategory added

---
+++ WHMS/Commands/Deleting/DeleteCategory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WHMS.Commands.Contracts;
+using WHMS.Services.Contracts;
+
+namespace WHMS.Commands.Deleting
+{
+    public class DeleteCategory : ICommand
+    {
+        ICategoryService categoryService;
+        IProductService productService;
+        public DeleteCategory(ICategoryService categoryService, IProductService productService)
+        {
+            this.categoryService = categoryService;
+            this.productService = productService;
+        }
+        public string Execute(IReadOnlyList<string> parameters)
+        {
+            var categoryNameToDelete = parameters[0];
+            var category = this.categoryService.FindByName(categoryNameToDelete);
+            if (this.productService.ProductsByCategory(category).Count > 0)
+            {
+                throw new ArgumentException($"There are products in {categoryNameToDelete}\n\rCAtegory cannot be deleted");
+            }
+            this.categoryService.DeleteCategory(categoryNameToDelete);
+            return $"Category {categoryNameToDelete} was deleted";
+
+        }
+    }
+}
